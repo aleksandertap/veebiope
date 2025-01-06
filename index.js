aba1723ef6e032ed48eeff45c199805aaf42880e
aleksandertap <aleksander.tapsi@voco.ee>
2025-01-06 18:20:56 +0200

    esimene pooleli

---
+++ index.js
@@ -0,0 +1,9 @@
+import getPosts, {getPostLenght} from "./postController.js";
+
+console.log(getPosts())
+console.log(`Post length: ${getPostLenght()}`)
+
+// const {generateRandomNumber, celciusToFahrenheit} = require("./utils");
+
+// console.log(`random number: ${generateRandomNumber()}`)
+// console.log(`Celcius to Fahrenheit: ${celciusToFahrenheit(0)}`)
