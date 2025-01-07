a57c305f6debf7834c4e73ec4bceeb8077f0f511
aleksandertap <aleksander.tapsi@voco.ee>
2025-01-07 18:20:16 +0200

    keeruline teema

---
+++ osDemo.js
@@ -0,0 +1,10 @@
+import os from 'os'
+
+//userInfo()
+console.log(os.userInfo())
+//totalmem()
+console.log(os.totalmem())
+//freemem()
+console.log(os.freemem())
+//cpus()
+console.log(os.cpus())
---
+++ processDemo.js
@@ -0,0 +1,29 @@
+//argv
+console.log(process.argv)
+console.log(process.argv[1])
+
+//porocess.env
+console.log(process.env)
+
+//pid
+console.log(process.pid)
+
+//cwd()
+console.log(process.cwd())
+
+//title
+console.log(process.title)
+
+//memoryUsage()
+console.log(process.memoryUsage())
+
+//update()
+console.log(process.uptime())
+
+process.on('exit',(code)=>{
+    console.log(`About to exit with code : ${code}`)
+})
+
+//exit
+process.exit(0)
+console.log('hello from after exit')
